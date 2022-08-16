a1c75fe9dbbebdbdb94fa331875c7db7de3a0889
Laura <bartekchaton@gmail.com>
2022-08-16 20:44:08 +0200

    std::cout tab fixed

--- main.cpp
+++ main.cpp
@@ -29,25 +29,25 @@ int main(int argc, char* argv[]) {
 	// DOS_HEADER
 	dosHeader = (PIMAGE_DOS_HEADER)fileData;
 	std::cout << "======== DOS HEADER ========\n";
-	std::cout << "Magic number: "						    << std::hex << dosHeader->e_magic		<< "\n";
-	std::cout << "Bytes on last page of file: "				<< std::hex << dosHeader->e_cblp		<< "\n";
-	std::cout << "Pages in file: "							<< std::hex << dosHeader->e_cp			<< "\n";
-	std::cout << "Relocations: "							<< std::hex << dosHeader->e_crlc		<< "\n";
-	std::cout << "Size of header in paragraphs: "			<< std::hex << dosHeader->e_cparhdr		<< "\n";
-	std::cout << "Minimum extra paragraphs needed: "		<< std::hex << dosHeader->e_minalloc	<< "\n";
-	std::cout << "Maximum extra paragraphs needed: "		<< std::hex << dosHeader->e_maxalloc	<< "\n";
-	std::cout << "Initial SS value: "						<< std::hex << dosHeader->e_ss			<< "\n";
-	std::cout << "tInitial SP value: "						<< std::hex << dosHeader->e_sp			<< "\n";
-	std::cout << "Initial SP value: "						<< std::hex << dosHeader->e_sp			<< "\n";
-	std::cout << "Checksum: "								<< std::hex << dosHeader->e_csum		<< "\n";
-	std::cout << "Initial IP value:	"						<< std::hex << dosHeader->e_ip			<< "\n";
-	std::cout << "Initial CS value: "						<< std::hex << dosHeader->e_cs			<< "\n";
-	std::cout << "File address of relocation table:"		<< std::hex << dosHeader->e_lfarlc		<< "\n";
-	std::cout << "Overlay number: "							<< std::hex << dosHeader->e_ovno		<< "\n";
-	std::cout << "OEM identifier: "							<< std::hex << dosHeader->e_oemid		<< "\n";
-	std::cout << "OEM identifier: "							<< std::hex << dosHeader->e_oemid		<< "\n";
-	std::cout << "OEM information: "						<< std::hex << dosHeader->e_oeminfo		<< "\n";
-	std::cout << "File address of new exe header: "			<< std::hex << dosHeader->e_lfanew		<< "\n";
+	std::cout << "Magic number: " << std::hex << dosHeader->e_magic << "\n";
+	std::cout << "Bytes on last page of file: " << std::hex << dosHeader->e_cblp << "\n";
+	std::cout << "Pages in file: " << std::hex << dosHeader->e_cp << "\n";
+	std::cout << "Relocations: " << std::hex << dosHeader->e_crlc << "\n";
+	std::cout << "Size of header in paragraphs: " << std::hex << dosHeader->e_cparhdr << "\n";
+	std::cout << "Minimum extra paragraphs needed: " << std::hex << dosHeader->e_minalloc << "\n";
+	std::cout << "Maximum extra paragraphs needed: " << std::hex << dosHeader->e_maxalloc << "\n";
+	std::cout << "Initial SS value: " << std::hex << dosHeader->e_ss << "\n";
+	std::cout << "tInitial SP value: " << std::hex << dosHeader->e_sp << "\n";
+	std::cout << "Initial SP value: " << std::hex << dosHeader->e_sp << "\n";
+	std::cout << "Checksum: " << std::hex << dosHeader->e_csum << "\n";
+	std::cout << "Initial IP value:	" << std::hex << dosHeader->e_ip << "\n";
+	std::cout << "Initial CS value: " << std::hex << dosHeader->e_cs << "\n";
+	std::cout << "File address of relocation table:" << std::hex << dosHeader->e_lfarlc << "\n";
+	std::cout << "Overlay number: " << std::hex << dosHeader->e_ovno << "\n";
+	std::cout << "OEM identifier: " << std::hex << dosHeader->e_oemid << "\n";
+	std::cout << "OEM identifier: " << std::hex << dosHeader->e_oemid << "\n";
+	std::cout << "OEM information: " << std::hex << dosHeader->e_oeminfo << "\n";
+	std::cout << "File address of new exe header: " << std::hex << dosHeader->e_lfanew << "\n";
 
 	return 0;
 }
